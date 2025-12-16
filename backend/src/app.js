83bb7d58cc0faa7b08092a411af0c62f62a19994
raghav(raghava-0650) <raghav83286@gmail.com>
2025-12-16 11:49:02 +0530

    backend setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,37 +1,27 @@
 import { createServer } from 'node:http';
 
-import cors from 'cors';
 import express from 'express';
 import mongoose from 'mongoose';
-
-import { connectToSocket } from './controllers/socketManager.js';
-import userRoutes from './routes/users.routes.js';
+import { Server } from 'socket.io';
 
 const app = express();
+
 const server = createServer(app);
-const io = connectToSocket(server);
+const io = new Server(server)
 
+app.set("port",(process.env.PORT || 8000));
 
-app.set("port", (process.env.PORT || 8000))
-app.use(cors());
-app.use(express.json({ limit: "40kb" }));
-app.use(express.urlencoded({ limit: "40kb", extended: true }));
+app.get("/home",(req, res) =>{
+    return res.json({ "hello":"world" })
+})
 
-app.use("/api/v1/users", userRoutes);
+const start = async()=>{
+    const connectiondb = await mongoose.connect("mongodb+srv://admin:admin@cluster0.kl33zyc.mongodb.net/");
 
-const start = async () => {
-    app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://imdigitalashish:imdigitalashish@cluster0.cujabk4.mongodb.net/")
-
-    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
-    server.listen(app.get("port"), () => {
-        console.log("LISTENIN ON PORT 8000")
+    console.log(`mongo connected db host: ${connectiondb.connection.host}`);
+    server.listen(app.get("port"),()=>{
+        console.log("listening on port 8000")
     });
-
-
-
 }
-
-
 
 start();
